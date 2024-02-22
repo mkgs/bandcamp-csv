f62ab9b3606d371346f6d5508e596a0dddceaca8
Michael Gisi <gisi@umich.edu>
2024-02-21 22:40:36 -0500

    lint

--- parse.py
+++ parse.py
@@ -7,31 +7,33 @@ from collections import defaultdict
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("csv_file", help="input .csv file", type=argparse.FileType('r', encoding='utf16'))
+parser.add_argument(
+    "csv_file", help="input .csv file", type=argparse.FileType("r", encoding="utf16")
+)
 args = parser.parse_args()
 reader = csv.reader(args.csv_file)
 
 header = True
-default = lambda: Decimal('0') 
+default = lambda: Decimal("0")
 data = defaultdict(default)
 quantity = defaultdict(int)
-shipping = Decimal('0')
-paypal_fees = Decimal('0')
-transfer_amount = Decimal('0')
+shipping = Decimal("0")
+paypal_fees = Decimal("0")
+transfer_amount = Decimal("0")
 pending_sales = False
 
 for row in reader:
     if header:
         try:
             header = False
-            net_amount_column = row.index('net amount')
-            catalog_number_column = row.index('catalog number')
-            shipping_column = row.index('shipping')
-            item_type_column = row.index('item type')
-            fee_type_column = row.index('fee type')
-            transaction_fee_column = row.index('transaction fee')
-            package_column = row.index('package')
-            quantity_column = row.index('quantity')
+            net_amount_column = row.index("net amount")
+            catalog_number_column = row.index("catalog number")
+            shipping_column = row.index("shipping")
+            item_type_column = row.index("item type")
+            fee_type_column = row.index("fee type")
+            transaction_fee_column = row.index("transaction fee")
+            package_column = row.index("package")
+            quantity_column = row.index("quantity")
         except ValueError:
             print("Error finding correct data in .csv files")
             sys.exit()
@@ -45,7 +47,7 @@ for row in reader:
         package = row[package_column]
 
         if catalog_number:
-            if item_type == 'pending sale':
+            if item_type == "pending sale":
                 pending_sales = True
             elif net_amount:
                 product_format = "vinyl" if "12''" in package else "digi"
@@ -55,7 +57,7 @@ for row in reader:
                 if shipping_amount:
                     shipping += Decimal(shipping_amount)
 
-        elif fee_type == 'transfer':
+        elif fee_type == "transfer":
             if transaction_fee:
                 paypal_fees += Decimal(transaction_fee)
 
@@ -70,4 +72,3 @@ print("Shipping\n    ${}\n".format(shipping))
 print("Paypal Fees\n    ${}\n".format(paypal_fees))
 print("Net Amount\n    ${}\n".format(transfer_amount))
 print("Pending Sales!!!" if pending_sales else "")
-
